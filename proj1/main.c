f3ffb71cd47083f07ccd334d2dfed1908d1d0e9d
Junyoung Kim <junyoung44@g.skku.edu>
2021-04-01 05:59:32 +0900

    R type instruction added

--- proj1/main.c
+++ proj1/main.c
@@ -36,6 +36,119 @@ void Rinst(char* inst){
     int rt=uint(inst+11,5);
     int rd=uint(inst+16,5);
     int sa=uint(inst+21,5);
+
+    if (!strncmp(inst+26,"100000",6)){
+        printf(" add $%d, $%d, $%d", rd,rs,rt);
+
+    }
+    else if (!strncmp(inst+26,"100001",6)){
+        printf(" addu $%d, $%d, $%d", rd,rs,rt);
+
+    }
+    else if (!strncmp(inst+26,"100100",6)){
+        printf(" and $%d, $%d, $%d", rd,rs,rt);
+
+    }
+    else if (!strncmp(inst+26,"011010",6)){
+        printf(" div $%d, $%d",rs,rt);
+
+    }
+    else if (!strncmp(inst+26,"011011",6)){
+        printf(" divu $%d, $%d",rs,rt);
+
+    }
+    else if (!strncmp(inst+26,"001001",6)){
+        printf(" jalr $%d, $%d", rd,rs);
+
+    }
+    else if (!strncmp(inst+26,"001000",6)){
+        printf(" jr $%d",rs);
+
+    }
+    else if (!strncmp(inst+26,"010000",6)){
+        printf(" mfhi $%d", rd);
+
+    }
+    else if (!strncmp(inst+26,"010010",6)){
+        printf(" mflo $%d", rd);
+
+    }
+    else if (!strncmp(inst+26,"010001",6)){
+        printf(" mthi $%d", rs);
+
+    }
+    else if (!strncmp(inst+26,"010011",6)){
+        printf(" mtlo $%d", rs);
+
+    }
+    else if (!strncmp(inst+26,"011000",6)){
+        printf(" mult $%d, $%d",rs,rt);
+
+    }
+    else if (!strncmp(inst+26,"011001",6)){
+        printf(" multu $%d, $%d",rs,rt);
+
+    }
+    else if (!strncmp(inst+26,"100111",6)){
+        printf(" nor $%d, $%d, $%d", rd,rs,rt);
+
+    }
+    else if (!strncmp(inst+26,"100101",6)){
+        printf(" or $%d, $%d, $%d", rd,rs,rt);
+
+    }
+    else if (!strncmp(inst+26,"000000",6)){
+        printf(" sll $%d, $%d, %d", rd,rt,sa);
+
+    }
+    else if (!strncmp(inst+26,"000100",6)){
+        printf(" sllv $%d, $%d, $%d", rd,rt,rs);
+
+    }
+    else if (!strncmp(inst+26,"101010",6)){
+        printf(" slt $%d, $%d, $%d", rd,rs,rt);
+
+    }
+    else if (!strncmp(inst+26,"101011",6)){
+        printf(" sltu $%d, $%d, $%d", rd,rs,rt);
+
+    }
+    else if (!strncmp(inst+26,"000011",6)){
+        printf(" sra $%d, $%d, $%d", rd,rt,sa);
+
+    }
+    else if (!strncmp(inst+26,"000111",6)){
+        printf(" srav $%d, $%d, $%d", rd,rt,rs);
+
+    }
+    else if (!strncmp(inst+26,"000010",6)){
+        printf(" srl $%d, $%d, $%d", rd,rt,sa);
+
+    }
+    else if (!strncmp(inst+26,"000110",6)){
+        printf(" srlv $%d, $%d, $%d", rd,rt,rs);
+
+    }
+    else if (!strncmp(inst+26,"100010",6)){
+        printf(" sub $%d, $%d, $%d", rd,rs,rt);
+
+    }
+    else if (!strncmp(inst+26,"100011",6)){
+        printf(" subu $%d, $%d, $%d", rd,rs,rt);
+
+    }
+    else if (!strncmp(inst+26,"001100",6)){
+        printf(" syscall");
+
+    }
+    else if (!strncmp(inst+26,"100110",6)){
+        printf(" xor $%d, $%d, $%d", rd,rs,rt);
+
+    }
+
+    else{
+        printf("unknown instruction");
+    }
 }
 
 void Iinst(char* inst){
